1d173616702e2a4acec58ca045aad578f5327f4f
MrTorstensson <MrTorstensson@outlook.com>
2017-08-07 10:36:28 +0000

    Added motor Limit

--- Motor.py
+++ Motor.py
@@ -7,10 +7,12 @@ import RPi.GPIO as GPIO
 
 class Motor:
     """ Will set up a PWM motor control """
-    def __init__(self, PORT = [11, 13]):
+    def __init__(self, PORT = [7, 8], Limit = 20):
         """__init__(self, PORT = [11, 13]), Will set up a PWM motor control on PORT = [11, 13] """
+        """ The limit added is the minimum pulsewidth that will be used on the motor, in order to avoid noize at low power """
         self.FWD = PORT[0]
         self.REW = PORT[1]
+        self.Limit = Limit
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.FWD, GPIO.OUT)
         GPIO.setup(self.REW, GPIO.OUT)
@@ -21,7 +23,10 @@ class Motor:
         
     def SPEED(self, Throttle):
         """SPEED(self, Throttle), Input float will set the Motor in a -100% to 100% speed range """
-        if Throttle < 0:
+        if abs(Throttle) < self.Limit:
+            self.fwd.ChangeDutyCycle(0)
+            self.rew.ChangeDutyCycle(0)
+        elif Throttle < 0:
             self.fwd.ChangeDutyCycle(0)
             self.rew.ChangeDutyCycle(-Throttle)
         else:
