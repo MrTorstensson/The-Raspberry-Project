f99dc2e658330925bb393c70f12a0f17e8a148cb
MrTorstensson <MrTorstensson@outlook.com>
2017-08-09 14:11:55 +0000

    Low Throttle frequenzy modulation  added

--- Motor.py
+++ Motor.py
@@ -4,10 +4,9 @@
 
 import RPi.GPIO as GPIO
 
-
 class Motor:
     """ Will set up a PWM motor control """
-    def __init__(self, PORT = [7, 8], Limit = 20):
+    def __init__(self, PORT = [7, 8], Limit = 40):
         """__init__(self, PORT = [11, 13]), Will set up a PWM motor control on PORT = [11, 13] """
         """ The limit added is the minimum pulsewidth that will be used on the motor, in order to avoid noize at low power """
         self.FWD = PORT[0]
@@ -24,12 +23,23 @@ class Motor:
     def SPEED(self, Throttle):
         """SPEED(self, Throttle), Input float will set the Motor in a -100% to 100% speed range """
         if abs(Throttle) < self.Limit:
-            self.fwd.ChangeDutyCycle(0)
-            self.rew.ChangeDutyCycle(0)
+            if(Throttle > 0):
+                self.fwd.ChangeFrequency(1./((100-self.Limit)*self.Limit/Throttle+self.Limit)*10000)
+                self.fwd.ChangeDutyCycle(self.Limit)
+                self.rew.ChangeDutyCycle(0)
+            elif(Throttle < 0):
+                self.rew.ChangeFrequency(1./((100-self.Limit)*self.Limit/-Throttle+self.Limit)*10000)
+                self.fwd.ChangeDutyCycle(0)
+                self.rew.ChangeDutyCycle(self.Limit)
+            else:
+                self.fwd.ChangeDutyCycle(0)
+                self.rew.ChangeDutyCycle(0)
         elif Throttle < 0:
+            self.rew.ChangeFrequency(100)
             self.fwd.ChangeDutyCycle(0)
             self.rew.ChangeDutyCycle(-Throttle)
         else:
+            self.fwd.ChangeFrequency(100)
             self.fwd.ChangeDutyCycle(Throttle)
             self.rew.ChangeDutyCycle(0)
 
