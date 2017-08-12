816528c979c628d828dd303dcbef13c1404bc0ba
MrTorstensson <MrTorstensson@outlook.com>
2017-08-12 18:56:12 +0000

    Cleanup of unused code

--- Motor.py
+++ Motor.py
@@ -4,46 +4,66 @@
 
 import RPi.GPIO as GPIO
 
-class Motor:
+class pwmMotor:
     """ Will set up a PWM motor control """
-    def __init__(self, PORT = [7, 8], Limit = 40):
+    def __init__(self, PORT = [11, 12], Limit = 40):
         """__init__(self, PORT = [11, 13]), Will set up a PWM motor control on PORT = [11, 13] """
         """ The limit added is the minimum pulsewidth that will be used on the motor, in order to avoid noize at low power """
-        self.FWD = PORT[0]
-        self.REW = PORT[1]
         self.Limit = Limit
+        self.PORT = PORT
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.FWD, GPIO.OUT)
-        GPIO.setup(self.REW, GPIO.OUT)
-        self.fwd = GPIO.PWM(self.FWD, 100)
-        self.rew = GPIO.PWM(self.REW, 100)
+        GPIO.setup(PORT[0], GPIO.OUT)
+        GPIO.setup(PORT[1], GPIO.OUT)
+        self.fwd = GPIO.PWM(PORT[0], 100)
+        self.rew = GPIO.PWM(PORT[1], 100)
         self.fwd.start(0)
         self.rew.start(0)
         
     def SPEED(self, Throttle):
         """SPEED(self, Throttle), Input float will set the Motor in a -100% to 100% speed range """
         if abs(Throttle) < self.Limit:
-            if(Throttle > 0):
-                self.fwd.ChangeFrequency(1./((100-self.Limit)*self.Limit/Throttle+self.Limit)*10000)
-                self.fwd.ChangeDutyCycle(self.Limit)
-                self.rew.ChangeDutyCycle(0)
-            elif(Throttle < 0):
-                self.rew.ChangeFrequency(1./((100-self.Limit)*self.Limit/-Throttle+self.Limit)*10000)
-                self.fwd.ChangeDutyCycle(0)
-                self.rew.ChangeDutyCycle(self.Limit)
-            else:
-                self.fwd.ChangeDutyCycle(0)
-                self.rew.ChangeDutyCycle(0)
+            self.fwd.ChangeDutyCycle(0)
+            self.rew.ChangeDutyCycle(0)
         elif Throttle < 0:
-            self.rew.ChangeFrequency(100)
             self.fwd.ChangeDutyCycle(0)
             self.rew.ChangeDutyCycle(-Throttle)
         else:
-            self.fwd.ChangeFrequency(100)
             self.fwd.ChangeDutyCycle(Throttle)
             self.rew.ChangeDutyCycle(0)
 
     def close(self):
         """ House keeping cleanup of GPIO ports, when motor is released """
-        GPIO.cleanup([self.FWD, self.REW])
+        self.fwd.stop()
+        self.rew.stop()
+        GPIO.cleanup(self.PORT)
         
+class TankMotor:
+    """ Will set up a PWM like motor control well suited for large motors to run slow but can have problem with rapid update < 0.5s """
+    def __init__(self, PORT = [11, 12]):
+        """__init__(self, PORT = [11, 13]), Will set up a PWM motor control on PORT = [11, 13] """
+        """ The limit added is the minimum pulsewidth that will be used on the motor, in order to avoid noize at low power """
+        self.PORT = PORT
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PORT[0], GPIO.OUT)
+        GPIO.setup(PORT[1], GPIO.OUT)
+        self.fwd = GPIO.PWM(PORT[0], 1)
+        self.rew = GPIO.PWM(PORT[1], 1)
+        self.fwd.start(0)
+        self.rew.start(0)
+        
+    def SPEED(self, Throttle):
+        """SPEED(self, Throttle), Input float will set the Motor in a -100% to 100% speed range """
+        if Throttle < 0:
+            self.rew.ChangeFrequency(max(1,abs(Throttle)))
+            self.fwd.ChangeDutyCycle(0)
+            self.rew.ChangeDutyCycle(min(100, -Throttle*2))
+        else:
+            self.fwd.ChangeFrequency(max(1,abs(Throttle)))
+            self.fwd.ChangeDutyCycle(min(100, Throttle*2))
+            self.rew.ChangeDutyCycle(0)
+
+    def close(self):
+        """ House keeping cleanup of GPIO ports, when motor is released """
+        self.fwd.stop()
+        self.rew.stop()
+        GPIO.cleanup(self.PORT)
